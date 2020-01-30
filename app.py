ce0849d144ca968f4cf44b60351c4e9d7081c891
rsbwork <47652041+rsbwork@users.noreply.github.com>
2020-01-30 21:30:51 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template 
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup 
+from io import BytesIO
+import base64
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+def scrap(url):
+    #This is fuction for scrapping
+    url_get = requests.get('https://monexnews.com/kurs-valuta-asing.htm?kurs=JPY&searchdatefrom=01-01-2019&searchdateto=31-12-2019')
+    soup = BeautifulSoup(url_get.content,"html.parser")
+    
+    #Find the key to get the information
+    table = soup.find('table') 
+    tr = table.find_all('tr') 
+
+    temp = [] #initiating a tuple
+
+    for i in range(1, len(tr)):
+        row = table.find_all('tr')[i]
+        #use the key to take information here
+        #name_of_object = row.find_all(...)[0].text
+        period = row.find_all('td')[0].text
+        period = period.strip() #for removing the excess whitespace
+    
+        #get inflasi
+        jual = row.find_all('td')[1].text
+        jual = jual.strip() #for removing the excess whitespace
+    
+        beli = row.find_all('td')[2].text
+        beli = beli.strip()
+    
+        temp.append((period,jual,beli)) 
+         #append the needed information 
+    
+    temp = temp[::-1] #remove the header
+
+    df = pd.DataFrame(temp, columns = ('period','jual','beli')) #creating the dataframe
+   #data wranggling -  try to change the data type to right data type
+    df['jual'] = df['jual'].str.replace(",",".")
+    df['jual'] = df['jual'].astype('float64')
+
+    df['beli'] = df['beli'].str.replace(",",".")
+    df['beli'] = df['beli'].astype('float64')
+
+    import dateparser
+    df['period'] = df.period.apply(lambda x: dateparser.parse(x))
+   #end of data wranggling
+    df=df.set_index('period')
+    return df
+
+@app.route("/")
+def index():
+    df = scrap('https://monexnews.com/kurs-valuta-asing.htm?kurs=JPY&searchdatefrom=01-01-2019&searchdateto=31-12-2019') #insert url here
+
+    #This part for rendering matplotlib
+   
+    df.plot()
+    
+    
+    #Do not change this part
+    plt.savefig('plot1',bbox_inches="tight") 
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result = str(figdata_png)[2:-1]
+    #This part for rendering matplotlib
+
+    #this is for rendering the table
+    df = df.to_html(classes=["table table-bordered table-striped table-dark table-condensed"])
+
+    return render_template("index.html", table=df, result=result)
+
+
+if __name__ == "__main__": 
+    app.run()
